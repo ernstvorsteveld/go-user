8760e5bf9af0c040e549af088bbb670b56b99138
ernst.vorsteveld <ernst.vorsteveld@gmail.com>
2019-10-06 04:41:19 -0400

    finished more stuff in the map repository

--- repository/user_map_repository.go
+++ repository/user_map_repository.go
@@ -6,21 +6,21 @@ import (
 )
 
 type userMapRepository struct {
-  idMap map[uuid.UUID]models.UserIdentity
-  usernameMap map[string]models.UserIdentity
-  emailMap map[string]models.UserIdentity
+  idMap map[uuid.UUID]*models.UserIdentity
+  usernameMap map[string]*models.UserIdentity
+  emailMap map[string]*models.UserIdentity
 }
 
 func NewMapUserRepository() *userMapRepository {
   return &userMapRepository{
-    idMap : make(map[uuid.UUID]models.UserIdentity),
-    usernameMap : make(map[string]models.UserIdentity),
-    emailMap : make(map[string]models.UserIdentity),
+    idMap : make(map[uuid.UUID]*models.UserIdentity),
+    usernameMap : make(map[string]*models.UserIdentity),
+    emailMap : make(map[string]*models.UserIdentity),
 	}
 }
 
 func (m *userMapRepository) GetById(id uuid.UUID) (*models.UserIdentity, error) {
-  return nil, nil
+  return m.idMap[id], nil
 }
 
 func (m *userMapRepository) GetByLoginCode(loginCode string) (*models.UserIdentity, error) { 
@@ -31,8 +31,10 @@ func (m *userMapRepository) Update(user *models.UserIdentity) (*models.UserIdent
     return nil, nil
 }
 
-func (m *userMapRepository) Create(a *models.UserIdentity) (uuid.UUID, error) {
-    return uuid.New(), nil
+func (m *userMapRepository) Create(u *models.UserIdentity) (uuid.UUID, error) {
+  u.User.Id = uuid.New()
+  m.idMap[u.User.Id] = u
+  return u.User.Id, nil
 }
 
 func (m *userMapRepository) Delete(id uuid.UUID) (bool, error) {
--- repository/user_map_repository_test.go
+++ repository/user_map_repository_test.go
@@ -4,9 +4,10 @@ import (
   "testing"
   models "github.com/ernstvorsteveld/go-user"
   "github.com/google/uuid"
+  "reflect"
 )
 
-func Test_implement_the_repository(t *testing.T) {
+func Test_create_and_retrieve_user(t *testing.T) {
   userRepository := NewMapUserRepository()
   
   pUser := &models.UserIdentity{
@@ -24,5 +25,15 @@ func Test_implement_the_repository(t *testing.T) {
   }
   if len(uuid) <= 0 {
     t.Errorf("When Creating a user in the repository, expected a UUID value.")
+  }
+  
+  pUserFromGet, errorFromGet := userRepository.GetById(uuid)
+  if errorFromGet != nil {
+    t.Errorf("Error returned by user repository while getting a user.")
+  }
+  
+  same := reflect.DeepEqual(*pUser, *pUserFromGet)
+  if !same {
+    t.Errorf("The retrieven user differs from the created user.")
   }
 }
